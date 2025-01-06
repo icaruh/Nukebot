c7081b09398f2b2a78e0d6b9cde7d9b53013b7bc
icaruh <onlinegamers008@gmail.com>
2025-01-06 13:08:56 +0000

    atualizado

--- obj/Debug/net9.0/DiscordBot.AssemblyInfo.cs
+++ obj/Debug/net9.0/DiscordBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33987b0171c15be20bf42387063cf84cf4289be")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
